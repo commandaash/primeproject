5740d11cd74fb24015708d541c146c7c43750bc4
Daniel Rupp <daniel@Jadine.Speedport_W723_V_Typ_A_1_01_022>
2020-05-15 23:13:02 +0200

    extended code to work up to 2nd degrre, but fails at 3rd

--- primeproject.py
+++ primeproject.py
@@ -1,24 +1,33 @@
 #var input
 input_set = range(1,101)
-degree = 2
+degree = 3
 
 #removing 0 and 1 from input_set
 set = [i for i in input_set if i !=0 and i !=1]
 
+#temp set for loop
+work_set = ([], set)
+
 #finds numbers prime in respect to their specific set
-def prime_maker(set):
+def prime_maker(x):
     set_np1 = []
-    for i in set:
-        for j in range(2, i):
-            if j in set:
+    m = list(x[1])
+    for i in m:
+        for j in range(m[0],i):
+            if j in m[0:-1]:
                 if i>1:
                     if i % j == 0:
                         set_np1.append(i)
                         break
-    set_p1 = [i for i in set if i not in set_np1]
-    return set_p1, set_np1
+    mesh_up = set_np1 + list(x[0])
+    set_p1 = [i for i in set if i not in mesh_up]
+    x = (set_p1, set_np1)
+    return x
 
-# if degree > 1:
+def prime_loop(x):
+    for k in range(degree):
+        x = prime_maker(x)
+    return x
 
-
-print(prime_maker(set)[0])
+print(prime_loop(work_set)[0])
+print(prime_loop(work_set)[1])
